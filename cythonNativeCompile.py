3da9fbdf725415fe1d16dfb2921f90324747b7e7
Chanaka Madushan <chanakalin@gmail.com>
2021-06-20 17:13:13 +0530

    HiLinkAPI re-written and optimized
    Thread implemented to keep session alive
    Reboot removed
    apiTest modified accordingly

--- cythonNativeCompile.py
+++ cythonNativeCompile.py
@@ -1,19 +1,19 @@
+#!/usr/bin/python3
+
 from distutils.core import setup
 from distutils.extension import Extension
+from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import glob, os
 
-ext_modules = [
-Extension("webui",  ["webui.py"]),
-]
+ext_modules = cythonize([
+Extension("HiLinkAPI",  ["HiLinkAPI.py"]),
+],
+compiler_directives=dict(always_allow_keywords=True, language_level='3')
+)
 
 setup(
-    name = 'huaweihilink',
+    name = 'hilinkapi',
     cmdclass = {'build_ext': build_ext},
     ext_modules = ext_modules
 )
-
-
-#delete C sources
-for sourceFile in glob.glob("*.c"):
-    os.remove(sourceFile)
